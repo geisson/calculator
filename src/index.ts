1a7dd075377a12870ed37b2780e6617ec93f6ea7
Geisson Iuri <geissoniuri@gmail.com>
2022-03-22 07:14:10 -0300

    feat(development calculus): developing the features to do the calculations

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,77 @@
 import '../sass/style.scss';
 
-const inputName = document.querySelector('[data-teste="nome"]') as HTMLInputElement;
-const btn = document.querySelector('[data-teste="btn"]') as HTMLButtonElement;
+const visorCurrentNumber = document.querySelector('[data-visor=current-number]') as HTMLDivElement;
+const visorAccumulator = document.querySelector('[data-visor=accumulator]') as HTMLDivElement;
+const buttons = document.querySelectorAll('button') as NodeListOf<HTMLButtonElement>;
 
-const handleClick = (event:Event):void => {
-  event.preventDefault();
+visorCurrentNumber.innerHTML = '';
+visorAccumulator.innerHTML = '';
+const maximumDisplayNumber: number = 8;
 
-  const inputValue = inputName.value;
+const elementsCalculateArray: Array<number| string> = [];
 
-  console.log(`Ola mundo ${inputValue}`);
-  console.log(this);
-  console.log(event);
+const handleKeyButton = (event: Event) => {
+  const keyButton = event.target as HTMLButtonElement;
+
+  const keyNumber = keyButton.dataset.number as string;
+  const keyOperator = keyButton.dataset.operator as string;
+  const keyFunctionality = keyButton.dataset.functionality as string;
+
+  const addDisplayNumber = (number:string) => {
+    const displaySize = visorCurrentNumber.innerHTML.length < maximumDisplayNumber;
+
+    if (displaySize) {
+      visorCurrentNumber.innerHTML += number;
+    }
+  };
+  if (keyNumber) addDisplayNumber(keyNumber);
+
+  const addComma = () => {
+    if (!visorCurrentNumber.innerHTML.includes('.')) {
+      visorCurrentNumber.innerHTML += '.';
+    }
+  };
+  if (keyFunctionality === 'comma') addComma();
+
+  const cleanCurrentEntry = () => {
+    visorCurrentNumber.innerHTML = '';
+  };
+  if (keyFunctionality === 'clear-element') cleanCurrentEntry();
+
+  const clearAll = () => {
+    visorAccumulator.innerHTML = '';
+    visorCurrentNumber.innerHTML = '';
+  };
+  if (keyFunctionality === 'clear') clearAll();
+
+  const handleOperation = () => {
+    const operator = keyButton.value;
+    const previousNumber = visorCurrentNumber.innerHTML;
+
+    if (previousNumber === '') return;
+
+    visorAccumulator.innerHTML = previousNumber + operator;
+    visorCurrentNumber.innerHTML = '';
+
+    elementsCalculateArray.push(parseFloat(previousNumber), operator);
+
+    const calculate = (arrayCalculate: Array<string | number>) => {
+      const number1 = arrayCalculate[0];
+      const number2 = arrayCalculate[2];
+      const signalOperator = arrayCalculate[1];
+
+      const verificaNumero = typeof number1 === 'number' && typeof number2 === 'number';
+
+      console.log(verificaNumero);
+
+      if (signalOperator === '+' && verificaNumero) console.log(number1 + number2);
+
+      console.log(arrayCalculate);
+      console.log(number1, number2, signalOperator);
+    };
+    calculate(elementsCalculateArray);
+  };
+  if (keyOperator) handleOperation();
 };
 
-btn?.addEventListener('click', handleClick);
+buttons.forEach((KeyButton) => KeyButton.addEventListener('click', handleKeyButton));
