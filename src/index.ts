142d5500f986cbbb6c35179f2497707ec2b1b271
Geisson Iuri <geissoniuri@gmail.com>
2022-03-24 07:13:27 -0300

    refactor(index.ts): refactors the old function into smaller functions

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,11 @@ import '../sass/style.scss';
 
 const visorCurrentNumber = document.querySelector('[data-visor=current-number]') as HTMLDivElement;
 const visorAccumulator = document.querySelector('[data-visor=accumulator]') as HTMLDivElement;
-// eslint-disable-next-line no-undef
-const buttons = document.querySelectorAll('button') as NodeListOf<HTMLButtonElement>;
+const keyNumbers = document.querySelectorAll('[data-number]') as NodeListOf<HTMLButtonElement>;
+const keyOperators = document.querySelectorAll('[data-operator]') as NodeListOf<HTMLButtonElement>;
+const keyComma = document.querySelector('[data-functionality=comma]') as HTMLButtonElement;
+const keyClearAll = document.querySelector('[data-functionality=clear-all]') as HTMLButtonElement;
+const keyClearElement = document.querySelector('[data-functionality=clear-element]') as HTMLButtonElement;
 
 visorCurrentNumber.innerHTML = '';
 visorAccumulator.innerHTML = '';
@@ -11,106 +14,97 @@ const maximumDisplayNumber: number = 8;
 
 const elementsCalculateArray: Array<number| string> = [];
 
-const handleKeyButton = (event: Event) => {
-  const keyButton = event.target as HTMLButtonElement;
+const handleKeyNumber = (event: Event) => {
+  const keyNumber = event.target as HTMLButtonElement;
+  const keyNumberValue = keyNumber.value;
 
-  const keyNumber = keyButton.dataset.number as string;
-  const keyOperator = keyButton.dataset.operator as string;
-  const keyFunctionality = keyButton.dataset.functionality as string;
-
-  const addDisplayNumber = (number:string) => {
+  const addDisplayNumber = (number: string) => {
     const displaySize = visorCurrentNumber.innerHTML.length < maximumDisplayNumber;
 
     if (displaySize) {
       visorCurrentNumber.innerHTML += number;
     }
   };
-  if (keyNumber) addDisplayNumber(keyNumber);
 
-  const addComma = () => {
-    if (!visorCurrentNumber.innerHTML.includes('.')) {
-      visorCurrentNumber.innerHTML += '.';
+  addDisplayNumber(keyNumberValue);
+};
+
+const handleKeyOperator = (event:Event) => {
+  const keyOperator = event.target as HTMLButtonElement;
+  const operator = keyOperator.value;
+  const previousNumber = visorCurrentNumber.innerHTML;
+
+  if (previousNumber === '') return;
+
+  visorAccumulator.innerHTML = previousNumber + operator;
+  visorCurrentNumber.innerHTML = '';
+
+  elementsCalculateArray.push(parseFloat(previousNumber), operator);
+
+  const calculation = (arrayCalculate: Array<string | number>) => {
+    const number1 = arrayCalculate[0];
+    const number2 = arrayCalculate[2];
+    const signalOperator = arrayCalculate[1];
+
+    const verificaNumero = typeof number1 === 'number' && typeof number2 === 'number';
+    const addition = signalOperator === '+' && verificaNumero;
+    const subtraction = signalOperator === '-' && verificaNumero;
+    const multiplication = signalOperator === '*' && verificaNumero;
+    const division = signalOperator === '/' && verificaNumero;
+
+    if (addition) {
+      const calculateSoma = number1 + number2;
+      visorCurrentNumber.innerHTML = `${calculateSoma}`;
     }
-  };
-  if (keyFunctionality === 'comma') addComma();
 
-  const cleanCurrentEntry = () => {
-    visorCurrentNumber.innerHTML = '';
-  };
-  if (keyFunctionality === 'clear-element') cleanCurrentEntry();
+    if (subtraction) {
+      const calculateSubtraction = number1 - number2;
+      visorCurrentNumber.innerHTML = `${calculateSubtraction}`;
+    }
 
-  const clearAll = () => {
-    visorAccumulator.innerHTML = '';
-    visorCurrentNumber.innerHTML = '';
-  };
-  if (keyFunctionality === 'clear') clearAll();
+    if (multiplication) {
+      const calculateMultiplication = number1 * number2;
+      visorCurrentNumber.innerHTML = `${calculateMultiplication}`;
+    }
 
-  const handleOperation = () => {
-    const operator = keyButton.value;
-    const previousNumber = visorCurrentNumber.innerHTML;
+    if (division) {
+      const calculateDivision = number1 / number2;
+      visorCurrentNumber.innerHTML = `${calculateDivision}`;
+    }
 
-    if (previousNumber === '') return;
+    if (arrayCalculate.length >= 4) {
+      arrayCalculate.splice(0, 4);
+    }
 
-    visorAccumulator.innerHTML = previousNumber + operator;
-    visorCurrentNumber.innerHTML = '';
-
-    elementsCalculateArray.push(parseFloat(previousNumber), operator);
-
-    const calculation = (arrayCalculate: Array<string | number>) => {
-      const number1 = arrayCalculate[0];
-      const number2 = arrayCalculate[2];
-      const signalOperator = arrayCalculate[1];
-
-      const verificaNumero = typeof number1 === 'number' && typeof number2 === 'number';
-      const addition = signalOperator === '+' && verificaNumero;
-      const subtraction = signalOperator === '-' && verificaNumero;
-      const multiplication = signalOperator === '*' && verificaNumero;
-      const division = signalOperator === '/' && verificaNumero;
-
-      if (addition) {
-        const calculateSoma = number1 + number2;
-        visorCurrentNumber.innerHTML = `${calculateSoma}`;
-      }
-
-      if (subtraction) {
-        const calculateSubtraction = number1 - number2;
-        visorCurrentNumber.innerHTML = `${calculateSubtraction}`;
-      }
-
-      if (multiplication) {
-        const calculateMultiplication = number1 * number2;
-        visorCurrentNumber.innerHTML = `${calculateMultiplication}`;
-      }
-
-      if (division) {
-        const calculateDivision = number1 / number2;
-        visorCurrentNumber.innerHTML = `${calculateDivision}`;
-      }
-
-      if (arrayCalculate.length >= 4) {
-        arrayCalculate.splice(0, 4);
-      }
-
-      console.log(`
+    console.log(`
         numero1= ${number1}
         numero2= ${number2},
         operação = ${signalOperator}
         array= ${arrayCalculate}`);
-    };
-    calculation(elementsCalculateArray);
+    console.log(arrayCalculate);
   };
-  if (keyOperator) handleOperation();
 
-  if (visorAccumulator.innerHTML.includes('=')) {
-    console.log('resultado final');
-    console.log(keyNumber);
-    console.log(keyButton);
-    const clickedNumber = document.querySelectorAll('[data-number]') as NodeListOf<HTMLButtonElement>;
-    clickedNumber.forEach((item) => item.addEventListener('click', () => {
-      visorCurrentNumber.innerHTML = '';
-      visorCurrentNumber.innerHTML = item.value;
-    }));
+  calculation(elementsCalculateArray);
+};
+
+const handleKeyComma = () => {
+  if (!visorCurrentNumber.innerHTML.includes('.')) {
+    visorCurrentNumber.innerHTML += '.';
   }
 };
 
-buttons.forEach((KeyButton) => KeyButton.addEventListener('click', handleKeyButton));
+const handleKeyClearAll = () => {
+  visorAccumulator.innerHTML = '';
+  visorCurrentNumber.innerHTML = '';
+};
+
+const handleKeyClearElement = () => {
+  visorCurrentNumber.innerHTML = '';
+};
+
+keyNumbers.forEach((numberButton) => numberButton.addEventListener('click', handleKeyNumber));
+keyOperators.forEach((keyOperator) => keyOperator.addEventListener('click', handleKeyOperator));
+keyComma.addEventListener('click', handleKeyComma);
+keyClearAll.addEventListener('click', handleKeyClearAll);
+keyClearElement.addEventListener('click', handleKeyClearElement);
+// buttons.forEach((KeyButton) => KeyButton.addEventListener('click', handleKeyButton));
